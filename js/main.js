569466b751d53d00eabd33a2da9124e43d851760
Rakesh Patel <emailme@rakeshrpatel.com>
2016-03-27 08:34:03 +0530

    almost finished

--- js/main.js
+++ js/main.js
@@ -1,15 +1,36 @@
 $(document).ready(function() {
-    $("#newitem_button").click(function() {
-        var newlistitem = $("input").val();
-        	$(".listitems ul").append("<li><i class='fa fa-check'></i><i class='fa fa-trash-o'></i>" + newlistitem + "</li>");
+    $("input:text").focus(function() {
+        $(this).select();
+
     });
 
+$("#newitem_button").on("click", function() {
+        var newlistitem = $("input").val();
+        $(".listitems ul").append("<li><i class='fa fa-check'></i><i class='fa fa-trash-o'></i><span>" + newlistitem + "</span></li>");
+        console.log("hey");
 
-    $(".list .fa-check").click(function(){
-    	$("").css("text-decoration", "line-through");
-    	//When icon is clicked, strike 
-    	// the text of the line item 
+
+
+        // $("#textbox").keypress(function(e) {
+        // if (e.which == 13) { //Enter key pressed
+        // $("#newitem_button").click(); //Trigger search button click event
+        // } });
+
+        $(".list .fa-check").on("click", function() {
+            $(this).siblings("span").toggleClass("toggle_strike");
+            console.log("hey");
+
+
+        });
+
+        $(".list .fa-trash-o").click(function() {
+            $(this).parent().fadeOut("fast", function() {
+                $(this).remove();
+            })
+
+        });
 
     });
 
 });
+
